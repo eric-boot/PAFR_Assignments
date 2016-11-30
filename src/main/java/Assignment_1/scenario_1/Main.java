27ce926255b2f219bef478ced6787c9be53b48d8
Eric Boot <eric.boot@student.hu.nl>
2016-11-30 11:30:58 +0100

    Added structure for 1st Assignment

---
+++ src/main/java/Assignment_1/scenario_1/Main.java
@@ -0,0 +1,7 @@
+package scenario_1;
+
+/**
+ * Created by Boot on 30-11-16.
+ */
+public class Main {
+}
---
+++ src/main/java/Assignment_1/scenario_2/Main.java
@@ -0,0 +1,7 @@
+package scenario_2;
+
+/**
+ * Created by Boot on 30-11-16.
+ */
+public class Main {
+}
