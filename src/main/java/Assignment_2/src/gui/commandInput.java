dca83045bd0562653f85d1b985fb8041e6d70f91
Maarten de Klerk <maarten.deklerk@gmail.com>
2017-01-02 20:51:10 +0100

    added command line input

---
+++ src/main/java/Assignment_2/src/gui/commandInput.java
@@ -0,0 +1,17 @@
+package Assignment_2.src.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Maarten de Klerk on 2-1-2017.
+ */
+public class commandInput {
+    private JPanel panel1;
+    private JTextField commandTextField;
+    private JLabel command;
+    private JButton executeButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
