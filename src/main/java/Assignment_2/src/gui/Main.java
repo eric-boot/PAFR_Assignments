3f580d487e8b5a379bf95c028c981fc7c12b6549
Maarten de Klerk <maarten.deklerk@gmail.com>
2017-01-02 21:37:54 +0100

    added main

---
+++ src/main/java/Assignment_2/src/gui/Main.java
@@ -0,0 +1,11 @@
+package Assignment_2.src.gui;
+
+/**
+ * Created by Maarten de Klerk on 2-1-2017.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        MainGUI mygui = new MainGUI();
+    }
+}
